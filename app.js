87f9b78de26ae994bdf4c7c82ea5d63fbe65f4d1
joshua <joshua.ar.jones@gmail.com>
2020-06-17 00:45:16 +0100

    user routes in place

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Routes
+app.use('/',require('./routes/index'));
+app.use('/users',require('./routes/users'));
 
-app.use('/',require('./routes/index'))
 
 
 app.listen(PORT,console.log(`Server started on port ${PORT} Josh`));
