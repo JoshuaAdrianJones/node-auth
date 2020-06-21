6cb4c01e9df9bd4634fe305cf89f9b7f45bccaf1
joshua <joshua.ar.jones@gmail.com>
2020-06-21 11:38:40 +0100

    ejs files setup

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// EJS 
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
 
-// Routes
+// Routes - tell the app where things are
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
 
 
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,console.log(`Server started on port ${PORT} Josh`));
-
-// commment
